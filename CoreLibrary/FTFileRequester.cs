a0b20c6c5ff807cdcac5b412f42c1e3a1f522dc8
Steven Vogelaar <steven.vogelaar@gmail.com>
2016-09-02 00:29:11 -0600

    + Can now download folders + Can now share multiple files/fodlers with the same name - Files now sent with a single socket connection

--- CoreLibrary/FTFileRequester.cs
+++ CoreLibrary/FTFileRequester.cs
@@ -14,15 +14,13 @@ namespace CoreLibrary
     {
 
         private Socket _socket;
-        private String _file;
+        private List<FTTFileInfo> _files;
         private Thread _senderThread;
         private String _dest;
-        private int _count;
 
-        public FTFileRequester(String file, Socket socket, String destDirectory)
+        public FTFileRequester(List<FTTFileInfo> files, Socket socket, String destDirectory)
         {
-            _count = 0;
-            _file = file;
+            _files = files;
             _socket = socket;
             _socket.ReceiveTimeout = 5000;
             _socket.SendTimeout = 5000;
@@ -34,18 +32,38 @@ namespace CoreLibrary
         }
 
         /// <summary>
-        /// Send the file request to the other client.
+        /// Send the files request to the other client.
         /// </summary>
         private void sendRequest()
         {
             try
             {
-                byte[] buffer = new byte[2048];
-                byte[] fName = Encoding.UTF8.GetBytes(_file);
-
-                for (int ii = 0; ii < fName.Length; ii++)
+                byte[] buffer = new byte[4096];
+                String[] fileNames = new String[_files.Count];
+                FTTFileInfo[] filesArray = _files.ToArray();
+                for (int i = 0; i < fileNames.Length; i++)
                 {
-                    buffer[ii] = fName[ii];
+                    fileNames[i] = filesArray[i].Name;
+                }
+
+                String combined = "";
+
+                // Combine names into single string
+                for (int i = 0; i < fileNames.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        combined = combined + "/" + fileNames[i];
+                    }
+                    else combined = combined + fileNames[i];
+                }
+
+                byte[] combinedBytes = Encoding.UTF8.GetBytes(combined);
+
+
+                for (int i = 0; i < combined.Length && i < buffer.Length; i++)
+                {
+                    buffer[i] = combinedBytes[i];
                 }
 
                 _socket.Send(buffer); 
@@ -89,6 +107,7 @@ namespace CoreLibrary
         private void receiveFile()
         {
             FileStream fileOut = null;
+            string fName = "";
 
             try
             {
@@ -97,8 +116,12 @@ namespace CoreLibrary
                 int received = 0;
                 long fileSize;
 
-                // Try to receive file name and number of chunks that are too be received for the file.
-                received = _socket.Receive(buffer);
+                // Try to receive file name and size of file to be received.
+                if ((received = _socket.Receive(buffer)) <= 0)
+                {
+                    return;
+                }
+
 
                 // Get first 8 bytes which is the file size.
                 byte[] sizeInBytes = new byte[8];
@@ -110,28 +133,33 @@ namespace CoreLibrary
                 // Convert first 8 bytes into long.
                 fileSize = BitConverter.ToInt64(sizeInBytes, 0);
 
-
                 // Get the filename which is in the rest of the 2048 bytes.
-                String fName = Encoding.UTF8.GetString(buffer, 8, 2039);
+                fName = Encoding.UTF8.GetString(buffer, 8, 2039);
                 fName = fName.Replace("\0", String.Empty);
 
                 FTTConsole.AddDebug("Filename received: " + fName);
 
                 // Get the file path without the file.
-                String path = fName.Substring(0,fName.LastIndexOf('\\'));
+                String path = _dest + "\\" + fName.Substring(0,fName.LastIndexOf('\\'));
                 // Create Directories for the path.
                 Directory.CreateDirectory(path);
  
                 // Create the output file.
                 fileOut = new FileStream(_dest + "\\" + fName, FileMode.Create);
 
-
                 // Write to the output file.
                 long bytesReceived = 0;
-                while ((received = _socket.Receive(buffer)) > 0 && bytesReceived < fileSize)
+
+                // First try to receive either the max chunk size or the filesize if it is smaller than the max chunk size.
+                int nextReceive = (int)Math.Min(2048, fileSize);
+
+                while (bytesReceived < fileSize && (received = _socket.Receive(buffer, nextReceive, SocketFlags.None )) > 0 )
                 {
                     fileOut.Write(buffer, 0, received);
-                    fileSize += received;
+                    bytesReceived += received;
+
+                    // This is so that it doesnt receive the header of the next file.
+                    nextReceive = (int)Math.Min(fileSize - bytesReceived, 2048);
                 }
             }
             catch (SocketException e)
@@ -143,15 +171,17 @@ namespace CoreLibrary
 
                 FTTConsole.AddError("Error receiving file: Socket Exception");
                 Console.WriteLine(e.Message + "\n" + e.StackTrace);
+                return;
             }
             catch (Exception e)
             {
 
                 FTTConsole.AddError("Error receiving file.");
                 Console.WriteLine(e.Message + "\n" + e.StackTrace);
+                return;
             }
 
-            FTTConsole.AddInfo("File received: " + _file);
+            FTTConsole.AddInfo("File received: " + fName);
 
             // Cleanup this instance.
             if (fileOut != null)
@@ -159,11 +189,14 @@ namespace CoreLibrary
                 fileOut.Close();
             }
 
+            // Try to receive another file if there is one.
+            receiveFile();
         }
 
 
         private void dispose()
         {
+            FTTConsole.AddDebug("Shutting down connection in requester");
             _socket.Shutdown(SocketShutdown.Both);
             _socket.Close();
         }
